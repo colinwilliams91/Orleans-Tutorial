d2bbd0106897cf78e753ab0a7ee6cd4448d8ab25
Colin Williams <colin.williams.dev@gmail.com>
2025-01-12 20:15:37 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Orleans.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// https://localhost:<port>/swagger/v1/swagger.json
+// https://localhost:<port>/swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!").WithOpenApi();
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+app.Run();
